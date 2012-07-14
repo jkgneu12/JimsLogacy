a81d1e629267773c9feae8bbe9b0f7147fee52c4
Jim George <jimgeorge89@gmail.com>
2012-07-14 00:56:53 -0400

    cleanup

--- src/plugin2/actions/DoStuff.java
+++ src/plugin2/actions/DoStuff.java
@@ -2,6 +2,7 @@ package plugin2.actions;
 
 import japa.parser.JavaParser;
 import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.ConstructorDeclaration;
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
 import japa.parser.ast.body.VariableDeclarator;
@@ -49,11 +50,11 @@ public class DoStuff implements IObjectActionDelegate {
 	private static final String TITLE = "Do Stuff";
 	private Shell shell;
 
-	String selectedText = null;
-	int selectedTextOffset = 0;
-	int length = 0;
+	private IEditorPart editorPart;
+	private IDocument doc;
+	
+	private String selectedText = null;
 
-	IEditorPart editorPart;
 	private int loggingStatementsPrinted;
 
 	/**
@@ -81,32 +82,15 @@ public class DoStuff implements IObjectActionDelegate {
 	public void run(IAction action) {
 		try {
 			loggingStatementsPrinted = 0;
-			//get active editor
+			
 			editorPart = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow()
 					.getActivePage().getActiveEditor();
 
 			if (editorPart instanceof AbstractTextEditor) {
-				//check if there is text selection
 
+				findSelectedText();
 
-				IEditorSite iEditorSite = editorPart.getEditorSite();
-				if (iEditorSite != null) {
-					ISelectionProvider selectionProvider = iEditorSite.getSelectionProvider();
-					if (selectionProvider != null) {
-						ISelection iSelection = selectionProvider.getSelection();
-						if (!iSelection.isEmpty()) {
-							selectedText = ((ITextSelection) iSelection).getText();
-						}
-					}
-				}
-
-				String textToReplace = "PyxisLog.e(Constants.PYXIS_LOG_TAG, \"" + selectedText + " = \" + " + selectedText + ");\n";
-
-				length = textToReplace.length();
-
-				ITextEditor editor = (ITextEditor) editorPart;
-				IDocumentProvider dp = editor.getDocumentProvider();
-				IDocument doc = dp.getDocument(editor.getEditorInput());
+				 findDocument();
 
 				CompilationUnit compUnit = JavaParser.parse(new ByteArrayInputStream(doc.get(0, doc.getLength()).getBytes()));
 
@@ -118,178 +102,160 @@ public class DoStuff implements IObjectActionDelegate {
 		}
 	}
 
+	private void findSelectedText() {
+		IEditorSite iEditorSite = editorPart.getEditorSite();
+		if (iEditorSite != null) {
+			ISelectionProvider selectionProvider = iEditorSite.getSelectionProvider();
+			if (selectionProvider != null) {
+				ISelection iSelection = selectionProvider.getSelection();
+				if (!iSelection.isEmpty()) {
+					selectedText = ((ITextSelection) iSelection).getText();
+				}
+			}
+		}
+	}
+	
+	private void findDocument() {
+		ITextEditor editor = (ITextEditor) editorPart;
+		IDocumentProvider dp = editor.getDocumentProvider();
+		doc = dp.getDocument(editor.getEditorInput());
+	}
+
 	public void printLog(ArrayList<String> vars, int endLineIndex){
 		endLineIndex += loggingStatementsPrinted - 1;
+		
 		try {
-			String textToReplace = "PyxisLog.e(Constants.PYXIS_LOG_TAG, ";
-			
-			for(int z = 0; z < vars.size(); z++){
-				String var = vars.get(z);
-				if(z > 0)
-					textToReplace += " + ";
-				textToReplace += "\" " + var + " = \" + " + var;
-			}
-			
-			textToReplace += ");\n";
-
-			length = textToReplace.length();
-
-			ITextEditor editor = (ITextEditor) editorPart;
-			IDocumentProvider dp = editor.getDocumentProvider();
-			IDocument doc = dp.getDocument(editor.getEditorInput());
-
-
-			int offset = doc.getLineOffset(endLineIndex); 
+			int lineOffset = doc.getLineOffset(endLineIndex); 
 
 			int endLineLength = doc.getLineLength(endLineIndex);
 
-			String endLine = doc.get(offset, endLineLength);
+			String endLine = doc.get(lineOffset, endLineLength);
+			
+			String textToInsert = buildTextToInsert(vars, endLineLength, endLine);
 
-			int tabs = 0;
-			for(int z = 0; z < endLineLength; z++){
-				if(endLine.charAt(z) == '\t')
-					textToReplace = "\t" + textToReplace;
+			lineOffset += endLineLength;
 
-			}
-
-			offset += doc.getLineLength(endLineIndex);
-
-			doc.replace(offset, 0, textToReplace);
+			doc.replace(lineOffset, 0, textToInsert);//insert the text into document
 
 		} catch (Exception e) {
 			MessageDialog.openError(shell, TITLE, e.getMessage());
 		}
+		
 		loggingStatementsPrinted++;
+	}
+
+	private String buildTextToInsert(ArrayList<String> vars, int endLineLength, String endLine) {
+		String textToInsert = "PyxisLog.e(Constants.PYXIS_LOG_TAG, ";
+		
+		for(int z = 0; z < vars.size(); z++){
+			String var = vars.get(z);
+			if(z > 0)
+				textToInsert += " + ";
+			textToInsert += "\" " + var + " = \" + " + var;
+		}
+		
+		textToInsert += ");\n";
+		
+		for(int z = 0; z < endLineLength; z++){
+			if(endLine.charAt(z) == '\t')
+				textToInsert = "\t" + textToInsert;
+		}
+		return textToInsert;
 	}
 
 	private class MethodVisitor extends VoidVisitorAdapter {
 
+		/**
+		 * Attempt to log all statements within Methods
+		 */
 		@Override
 		public void visit(MethodDeclaration n, Object arg) {
-			// here you can access the attributes of the method.
-			// this method will be called for all methods in this 
-			// CompilationUnit, including inner class methods
-			System.out.println("MethodDeclaration " + n.getName());
-			
-			BlockStmt body = n.getBody();
-			iterateOverStatements(body.getStmts());
+			attemptToLogStatements(n.getBody().getStmts(), n.getName());
 		}
 		
-		private void iterateOverStatements(List<Statement> stmts){
+		/**
+		 * Attempt to log all statements within Constructors
+		 */
+		@Override
+		public void visit(ConstructorDeclaration n, Object arg) {
+			attemptToLogStatements(n.getBlock().getStmts(), n.getName());
+		}
+		
+		/**
+		 * Attempt to log all statements in a list
+		 * @param stmts - List of Statements that will attempt to be logged
+		 * @param methodName - name of method that the Statements are in
+		 */
+		private void attemptToLogStatements(List<Statement> stmts, String methodName){
 			Iterator<Statement> iterator = stmts.iterator();
 			while(iterator.hasNext()){
 				Statement stmt = iterator.next();
 				if(stmt instanceof ExpressionStmt){
 					Expression exp = ((ExpressionStmt)stmt).getExpression();
-					if(exp instanceof VariableDeclarationExpr){
-						parse((VariableDeclarationExpr)exp);
-					} else if (exp instanceof AssignExpr){
-						parse((AssignExpr)exp);
-					}
+					breakDownExpressionAndAttemptToLog(exp, methodName);
 				}
 			}
 		}
-
-		@Override
-		public void visit(FieldAccessExpr n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-		}
-
-		@Override
-		public void visit(FieldDeclaration n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-			parse(n);
-		}
 		
-		@Override
-		public void visit(VariableDeclarationExpr n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-			parse(n);
-			
-		}
-
-		@Override
-		public void visit(ExpressionStmt n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-		}
-		
-		@Override
-		public void visit(AssignExpr n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-			parse(n);
-		}
-		
-		@Override
-		public void visit(VariableDeclarator n, Object arg) {
-			// TODO Auto-generated method stub
-			super.visit(n, arg);
-		}
-		
-		private void parse(FieldDeclaration exp){
-			if(selectedText.contains(exp.toString())){
-				iterateOverVars(exp.getVariables(), exp.getEndLine());
-			}
-		}
-		
-		private void parse(VariableDeclarationExpr exp){
-			if(selectedText.contains(exp.toString())){
-				iterateOverVars(exp.getVars(), exp.getEndLine());
-			}
-		}
-		
-		private void parse(AssignExpr exp){
-			if(selectedText.contains(exp.toString())){
+		/**
+		 * Attempt to log an Expression
+		 * @param exp - variable declaration that will attempt to be logged
+		 * @param methodName - name of method that the expression is in
+		 */
+		private void breakDownExpressionAndAttemptToLog(Expression exp, String methodName){
+			if(shouldLog(exp.toString(), methodName)){
 				ArrayList<String> vars = new ArrayList<String>();
-				addExpParts(vars, exp.getTarget());
-				addExpParts(vars, exp.getValue());
+				addExpressionParts(vars, exp);
+				
 				printLog(vars, exp.getEndLine());
 			}
 		}
-
-		private void iterateOverVars(List<VariableDeclarator> varDecs, int endLineIndex){
-			ArrayList<String> vars = new ArrayList<String>();
-			
-			ListIterator<VariableDeclarator> iterator = varDecs.listIterator();
-			while(iterator.hasNext()){
-				VariableDeclarator dec = iterator.next();
-				vars.add(dec.getId().toString());
-				Expression exp = dec.getInit();
-				addExpParts(vars, exp);
-			}
-			printLog(vars, endLineIndex);
-		}
 		
-		private void addExpParts(ArrayList<String> vars, Expression exp) {
+		/**
+		 * Should this Expression be logged?
+		 * @param exp - expression's text
+		 * @param methodName - name of method that the expression is in
+		 */
+		private boolean shouldLog(String exp, String methodName) {
+			return selectedText.contains(exp) || selectedText.contains(methodName);
+		}
+
+		
+		/**
+		 * Add parts of an Expression to a List
+		 * @param vars - list to add parts to
+		 * @param exp - expression to get parts from
+		 */
+		private void addExpressionParts(ArrayList<String> vars, Expression exp) {
 			if(exp instanceof NameExpr){
 				vars.add(exp.toString());
 			}
 			else if(exp instanceof BinaryExpr){
-				Expression left = ((BinaryExpr)exp).getLeft();
-				Expression right = ((BinaryExpr)exp).getRight();
-				addExpParts(vars, left);
-				addExpParts(vars, right);
+				BinaryExpr binaryExpr = (BinaryExpr)exp;
+				Expression left = binaryExpr.getLeft();
+				Expression right = binaryExpr.getRight();
+				addExpressionParts(vars, left);
+				addExpressionParts(vars, right);
 			}
 			else if(exp instanceof AssignExpr){
-				vars.add(((AssignExpr)exp).getTarget().toString());
-				addExpParts(vars, ((AssignExpr)exp).getValue());
+				AssignExpr assignExpr = (AssignExpr)exp;
+				vars.add(assignExpr.getTarget().toString());
+				addExpressionParts(vars, assignExpr.getValue());
+			} 
+			else if(exp instanceof VariableDeclarationExpr) {
+				List<VariableDeclarator> varDecs = ((VariableDeclarationExpr)exp).getVars();
+				ListIterator<VariableDeclarator> iterator = varDecs.listIterator();
+				while(iterator.hasNext()){
+					VariableDeclarator dec = iterator.next();
+					vars.add(dec.getId().toString());
+					addExpressionParts(vars, dec.getInit());
+				}
 			}
 		}
 	}
 
-
-
-	/**
-	 * Skip implementation
-	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
-	 */
 	@Override
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
+	public void selectionChanged(IAction action, ISelection selection) {}
 
 }
 
